97c9f73d3e36da5f80226928fc14bf4fb2c1afb0
klaus <klaus@hephaisto-laptop>
2015-02-04 00:23:54 +0100

    test: material width

--- testOpenPyCAD.py
+++ testOpenPyCAD.py
@@ -18,30 +18,32 @@ if not len(sys.argv)==2:
 	print("available metrics: {}".format(",".join([str(m) for m in metrics.keys()])))
 	sys.exit()
 
+material_width=3.0
 def testCube(m):
-	side_size=max(m.ac+2*5.0,m.headHeight+m.nutHeight+5.0)
+	side_size=max(m.ac+2*material_width,m.headHeight+m.nutHeight+material_width)
+        print(side_size)
 	return (group()
 		(
-			cube(3*m.ac+4*5.0,side_size,side_size)
+			cube(1*side_size,side_size,side_size)
 			,visibleHole
 			(
 				m.screw_from_to(
-					side_size/2+0*(m.ac+5.0) ,side_size/2, 0,
-					side_size/2+0*(m.ac+5.0), side_size/2, side_size,
+					side_size/2+0*(m.ac+material_width) ,side_size/2, 0,
+					side_size/2+0*(m.ac+material_width), side_size/2, side_size,
 					head="hex",nut="round")
 			)
 			
 			,visibleHole
 			(
-				m.screw_from_to(side_size/2+1*(m.ac+5.0) ,0, side_size/2,
-					 side_size/2+1*(m.ac+5.0), side_size, side_size/2,
+				m.screw_from_to(side_size/2+1*(m.ac+material_width) ,0, side_size/2,
+					 side_size/2+1*(m.ac+material_width), side_size, side_size/2,
 					 head="hex",nut="round")
 			)
 			
 			,visibleHole
 			(
-				m.screw_from_to(side_size/2+2*(m.ac+5.0) ,side_size/2, side_size,
-					 side_size/2+2*(m.ac+5.0), side_size/2, 0,
+				m.screw_from_to(side_size/2+2*(m.ac+material_width) ,side_size/2, side_size,
+					 side_size/2+2*(m.ac+material_width), side_size/2, 0,
 					 head="hex",nut="round")
 			)
 		)
